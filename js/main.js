1210483fcf73b60ce0e2250bbdc1859b5d5129f6
marwanammar <marwanwael775@gmail.com>
2024-01-30 14:34:59 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+let inputTitle = document.getElementById("title-note"),
+  add = document.getElementById("add"),
+  inputText = document.getElementById("content-note"),
+  output = document.getElementById("output");
+
+//output
+let item, btndel, h2, p;
+//no note alert
+add.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if ((inputTitle.value == "") | (inputTitle.value == " ")) {
+    alert("make sure that u have written a note");
+  } else if ((inputText.value == "") | (inputText.value == " ")) {
+    alert("make sure that u have written details");
+  } else {
+    item = document.createElement("div");
+    btndel = document.createElement("button");
+    h2 = document.createElement("h2");
+    p = document.createElement("p");
+    //give the notes the value
+    h2.innerHTML = inputText.value;
+    p.innerHTML = inputText.value;
+    btndel.innerHTML = "delete";
+    //add class
+    item.classList.add("item");
+    btndel.classList.add("remove");
+    h2.classList.add("title-note");
+    p.classList.add("content-note");
+    //هنضيف جوا ال div
+    item.appendChild(btndel);
+    item.appendChild(p);
+    item.appendChild(h2);
+    //use the output
+    output.appendChild(item);
+  }
+});
